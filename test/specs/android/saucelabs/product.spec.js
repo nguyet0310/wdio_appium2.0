620a556ef0126a5dcf341cd074312e2b3decbec2
Moon Nguyen <moon.nguyen@ganjing.dev>
2024-02-28 07:05:20 +0700

    add comments for testcase

--- test/specs/android/saucelabs/product.spec.js
+++ test/specs/android/saucelabs/product.spec.js
@@ -8,17 +8,24 @@ import { faker } from '@faker-js/faker';
 
 describe('Product Test', ()=>{
     it('Add products to cart successfully', async ()=>{
+        // Login to app
         await LoginScreen.login('standard_user', 'secret_sauce')
+        // Assert that product page display
         await expect(ProductScreen.productTitle).toBeDisplayed();
+        // Open a product with name and then add this product to cart
         await ProductScreen.openProduct('Sauce Labs Backpack')
         await Gesture.scrollUntilTextVisible('ADD TO CART')
         await ProductDetailScreen.clickAddToCartBtn()
+        // Back to product list page
         await ProductDetailScreen.clickBackBtn()
         await driver.pause(10000)
+        // Add second product to cart
         await ProductScreen.clickAddToCartBtn()
+        // Go to cart
         await ProductScreen.clickCartBtn()
        
         await Gesture.scrollUntilTextVisible('CHECKOUT')
+        // Check out product
         await CartScreen.clickCheckoutBtn()
         console.log('Start filling checkout address')
         await CheckoutScreen.fillFistname(faker.person.firstName())
